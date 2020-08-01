d383be6f000714fbc4b5b9b9e0de0a4e20302514
cheffplayer <dakrav51@gmail.com>
2020-08-01 03:37:30 -0700

    test

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,20 +1,7 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	pass
 
 func _on_Button_pressed():
 	if AudioServer.is_bus_mute(1) == false:
@@ -23,4 +10,3 @@ func _on_Button_pressed():
 	else:
 		AudioServer.set_bus_mute(1,false)
 		print('audio unmuted')
-
