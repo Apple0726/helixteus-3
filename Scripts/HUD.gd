c3c37483c3e74c7bb0fedd8dde42b6a4f5c7ff98
cheffplayer <dakrav51@gmail.com>
2020-08-08 22:41:49 -0700

    unneeded code removed

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,12 +1,7 @@
 extends Control
 
-func _ready():
-	pass
-
 func _on_Button_pressed():
 	if AudioServer.is_bus_mute(1) == false:
 		AudioServer.set_bus_mute(1,true)
-		print('audio muted')
 	else:
 		AudioServer.set_bus_mute(1,false)
-		print('audio unmuted')
