ddf57a9838471fd92078786ca15f8552a035d81d
cheffplayer <dakrav51@gmail.com>
2021-12-17 05:27:38 -0800

    Removed print

--- Scripts/View.gd
+++ Scripts/View.gd
@@ -477,9 +477,10 @@ func _input(event):
 
 #Zooming code
 func _zoom_at_point(zoom_change, center:Vector2 = mouse_position):
+	#if limit_to_viewport and is_instance_valid(obj) and obj.dimensions and scale.x < 250 / obj.dimensions and zoom_change < 1:
+	#	return
 	var max_zoom_out = 250 / obj.dimensions
 	var max_zoom_in = 5000 / obj.dimensions
-	print(limit_to_viewport, zoom_change)
 	if zoom_change < 1 and scale.x < max_zoom_out:
 		return
 	if zoom_change > 1 and scale.x > max_zoom_in:
